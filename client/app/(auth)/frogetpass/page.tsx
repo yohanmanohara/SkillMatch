7176c6e6478820d49d8ab7975fe2cf4df5165076
yohanmanohara <warushayohan80@gmail.com>
2024-09-26 10:36:16 +0530

    #

--- client/app/(auth)/frogetpass/page.tsx
+++ client/app/(auth)/frogetpass/page.tsx
@@ -1,7 +1,7 @@
 
 import { Metadata } from 'next';
 import Link from 'next/link';
-import FrogetAuthForm from '@/components/form/frogetAuthForm';
+import FrogetAuthForm from '@/components/forms/frogetAuthForm';
 import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
