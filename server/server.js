d65c1a5c6c241102c1422f91b3d088caa3130c2f
yohanmanohara <warushayohan80@gmail.com>
2024-09-26 10:38:44 +0530

    #

---
+++ server/server.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+const cors = require('cors');
+const express = require('express')
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', true);
+const userRoutes = require('./routes/user');
+const useVehicle = require('./routes/vehicle');
+const app = express()
+app.use(cors()); 
+app.use(express.json())
+
+
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+
+
+app.use('/api/user', userRoutes);
+
+app.use('/api/vehicle',useVehicle);
+  
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path}`);
+  next();
+});
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log('Mono db scussefully & listening on port', process.env.PORT)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+
+  console.log("Server is running on port 4000");
+
+
+
