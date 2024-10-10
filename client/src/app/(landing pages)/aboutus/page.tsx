a7cb7fc45bb592fb3046f60196ae1a2e622e6bc3
Yashira-De-Silva <yashiradesilva@gmail.com>
2024-10-10 15:27:59 +0530

    #

---
+++ client/src/app/(landing pages)/aboutus/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div> about us page</div>
+  )
+}
+
+export default page
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,7 +4,8 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-     landing page
+      kkk
+   
     </div>
   )
 }
