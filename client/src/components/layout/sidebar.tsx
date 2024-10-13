f4468ad7baf4b5a3ca69bc4b66c1a5a0b85e43c4
yohanmanohara <warushayohan80@gmail.com>
2024-10-13 12:23:13 +0530

    Add switchin button to emp and emleee

--- client/src/components/layout/sidebar.tsx
+++ client/src/components/layout/sidebar.tsx
@@ -1,38 +1,52 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { DashboardNav } from '@/components/ui/dashboard-nav';
-import {  navItem } from '@/constants/data';
+import { navItem } from '@/constants/data';
 import { cn } from '@/lib/utils';
-import { ChevronLeft } from 'lucide-react';
 import { useSidebar } from '@/hooks/useSidebar';
-import Link from 'next/link';
-
+import { Button } from '@/components/ui/button';
+import {useRouter} from 'next/navigation';
 type SidebarProps = {
   className?: string;
 };
 
 export default function Sidebar({ className }: SidebarProps) {
-  const { isMinimized, toggle } = useSidebar();
+  const { isMinimized } = useSidebar();
+  const [showButton, setShowButton] = useState(false);
+  const router = useRouter();
+  useEffect(() => {
+    const roleData = sessionStorage.getItem('role');
+    const role = roleData ? JSON.parse(roleData).role : null;
+    
+    if (role === 'Employer') {
+      setShowButton(true);
+    } 
+  }, []); 
 
- 
-
+  
   return (
     <aside
       className={cn(
-        `fixed  hidden h-screen flex-none border-r bg-card transition-[width] duration-500 md:block `,
+        `fixed hidden h-screen flex-none border-r bg-card transition-[width] duration-500 md:block`,
         !isMinimized ? 'w-72' : 'w-[72px]',
         className
-
       )}
     >
-      <div className="hidden p-5  pt-10 lg:block">
-      LoGO
+      <div className="hidden p-5 pt-10 lg:block">
+        LoGO
       </div>
- 
+
       <div className="space-y-4 py-4 ">
         <div className="px-3 py-2 ">
           <div className="mt-3 space-y-1 ">
             <DashboardNav items={navItem} />
+
+            <div className='pt-7 pl-6'>
+            {showButton && (
+              <Button variant="secondary" onClick={() => router.push('/employer/overview')}>Employer View</Button> // Update button text as needed
+            )}
+            </div>
+           
           </div>
         </div>
       </div>
