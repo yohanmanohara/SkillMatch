1f9ff6056a1663ab912f5089af05912b2669f992
NanayakkaraAR <ashinireshana@gmail.com>
2024-10-14 20:01:02 +0530

    added cards to the features section

---
+++ client/src/app/(landing pages)/features/page.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+
+function page() {
+  return (
+    <>
+    
+    <div> 
+        <h1>Features</h1>
+    </div>
+
+    <div className='flex flex-row justify-between'>
+        
+        <div className="card bg-green-200 style={{ backgroundColor: '#DCFCE0' }} text-neutral-content w-80">
+        <div className="card-body text-left text-gray-900">
+            <h2 className="card-title">Basic dialog title</h2>
+            <div className='text-sm'>A dialog is a type of modal window that appears in front of app content to provide critical information, or prompt for a decision to be made.</div>
+        <div className="card-actions justify-end gap-8">
+            <div>Action 2</div>
+            <div>Action 1</div>
+        </div>
+        </div>
+        </div>
+        
+
+        <div className="card bg-green-200 style={{ backgroundColor: '#DCFCE0' }} text-neutral-content w-80">
+        <div className="card-body text-left text-gray-900">
+            <h2 className="card-title">Basic dialog title</h2>
+            <div className='text-sm'>A dialog is a type of modal window that appears in front of app content to provide critical information, or prompt for a decision to be made.</div>
+        <div className="card-actions justify-end gap-8">
+            <div>Action 2</div>
+            <div>Action 1</div>
+        </div>
+        </div>
+        </div>
+
+
+        <div className="card bg-green-200 style={{ backgroundColor: '#DCFCE0' }} text-neutral-content w-80">
+        <div className="card-body text-left text-gray-900">
+            <h2 className="card-title">Basic dialog title</h2>
+            <div className='text-sm'>A dialog is a type of modal window that appears in front of app content to provide critical information, or prompt for a decision to be made.</div>
+        <div className="card-actions justify-end gap-8">
+            <div>Action 2</div>
+            <div>Action 1</div>
+        </div>
+        </div>
+        </div>
+
+
+        <div className="card bg-green-200 style={{ backgroundColor: '#DCFCE0' }} text-neutral-content w-80">
+        <div className="card-body text-left text-gray-900">
+            <h2 className="card-title">Basic dialog title</h2>
+            <div className='text-sm'>A dialog is a type of modal window that appears in front of app content to provide critical information, or prompt for a decision to be made.</div>
+        <div className="card-actions justify-end gap-8">
+            <div>Action 2</div>
+            <div>Action 1</div>
+        </div>
+        </div>
+        </div>
+    </div>
+
+    </>
+  )
+}
+
+export default page
