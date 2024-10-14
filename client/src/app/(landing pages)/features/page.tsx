730a1b69f82930743b40fc0feef168c6b7b3b22d
NanayakkaraAR <ashinireshana@gmail.com>
2024-10-14 20:43:14 +0530

    added a scroll bar

--- client/src/app/(landing pages)/features/page.tsx
+++ client/src/app/(landing pages)/features/page.tsx
@@ -4,13 +4,15 @@ function page() {
   return (
     <>
     
-    <div> 
+    <div className='mb-6'> 
         <h1>Features</h1>
     </div>
 
-    <div className='flex flex-row justify-between'>
+    <div className='overflow-x-auto'>
+    
+    <div className='flex space-x-4 p-4 w-max' >
         
-        <div className="card bg-green-200 style={{ backgroundColor: '#DCFCE0' }} text-neutral-content w-80">
+        <div className="card bg-green-200 style={{ backgroundColor: '#DCFCE0' }} text-neutral-content w-80 ">
         <div className="card-body text-left text-gray-900">
             <h2 className="card-title">Basic dialog title</h2>
             <div className='text-sm'>A dialog is a type of modal window that appears in front of app content to provide critical information, or prompt for a decision to be made.</div>
@@ -56,6 +58,20 @@ function page() {
         </div>
         </div>
         </div>
+
+
+        <div className="card bg-green-200 style={{ backgroundColor: '#DCFCE0' }} text-neutral-content w-80">
+        <div className="card-body text-left text-gray-900">
+            <h2 className="card-title">Basic dialog title</h2>
+            <div className='text-sm'>A dialog is a type of modal window that appears in front of app content to provide critical information, or prompt for a decision to be made.</div>
+        <div className="card-actions justify-end gap-8">
+            <div>Action 2</div>
+            <div>Action 1</div>
+        </div>
+        </div>
+        </div>
+
+    </div>
     </div>
 
     </>
