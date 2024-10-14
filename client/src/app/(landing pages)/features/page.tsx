293cdaa26368348ca01d17930a58e2a6fbb53aa8
NanayakkaraAR <ashinireshana@gmail.com>
2024-10-14 20:54:41 +0530

    completed the features section

--- client/src/app/(landing pages)/features/page.tsx
+++ client/src/app/(landing pages)/features/page.tsx
@@ -71,6 +71,18 @@ function page() {
         </div>
         </div>
 
+
+        <div className="card bg-green-200 style={{ backgroundColor: '#DCFCE0' }} text-neutral-content w-80">
+        <div className="card-body text-left text-gray-900">
+            <h2 className="card-title">Basic dialog title</h2>
+            <div className='text-sm'>A dialog is a type of modal window that appears in front of app content to provide critical information, or prompt for a decision to be made.</div>
+        <div className="card-actions justify-end gap-8">
+            <div>Action 2</div>
+            <div>Action 1</div>
+        </div>
+        </div>
+        </div>
+
     </div>
     </div>
 
