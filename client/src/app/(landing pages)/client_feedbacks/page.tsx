e555825ac4753f517879fbc850da2f36864b1f84
NanayakkaraAR <ashinireshana@gmail.com>
2024-10-16 10:09:07 +0530

    edited client feedbacks

--- client/src/app/(landing pages)/client_feedbacks/page.tsx
+++ client/src/app/(landing pages)/client_feedbacks/page.tsx
@@ -7,25 +7,25 @@ function page() {
         <div className="flex justify-between text-center space-x-10">
 
           <div className="flex flex-col">
-            <h2 className="text-6xl font-bold text-gray-800 mb-3">98.45%</h2>
-            <p className="text-gray-700 font-bold">Positive Feedbacks</p>
-            <p className='text-gray-700 text-sm'>From our Doctors</p>
+            <h2 className="text-6xl font-bold text-gray-900 mb-3">98.45%</h2>
+            <p className="text-gray-900 font-bold">Positive Feedbacks</p>
+            <p className='text-gray-900 text-sm'>From our Doctors</p>
           </div>
 
           <div className="border-l border-gray-300 mx-6"></div>
 
           <div className="flex flex-col">
-            <h2 className="text-6xl font-bold text-gray-800 mb-3">3000+</h2>
-            <p className="text-gray-700 font-bold">Questions & Answers</p>
-            <p className='text-gray-700 text-sm'>Your Questions</p>
+            <h2 className="text-6xl font-bold text-gray-900 mb-3">3000+</h2>
+            <p className="text-gray-900 font-bold">Questions & Answers</p>
+            <p className='text-gray-900 text-sm'>Your Questions</p>
           </div>
 
           <div className="border-l border-gray-300 mx-6"></div>
           
             <div className='flex flex-col'>
-            <h2 className="text-6xl font-bold text-gray-800 mb-3">2,500+</h2>
-            <p className="text-gray-700 font-bold">Experienced Professors</p>
-            <p className='text-gray-700 text-sm'>High Qualified </p>
+            <h2 className="text-6xl font-bold text-gray-900 mb-3">2,500+</h2>
+            <p className="text-gray-900 font-bold">Experienced Professors</p>
+            <p className='text-gray-900 text-sm'>High Qualified </p>
             </div>
             
           
