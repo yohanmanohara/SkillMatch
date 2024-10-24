e661a0c67cf372582045f7937f812b7d631c9e92
yohanmanohara <warushayohan80@gmail.com>
2024-10-24 12:00:07 +0530

    #

--- main_server/models/userModel.js
+++ main_server/models/userModel.js
@@ -58,7 +58,7 @@ const userSchema = new Schema({
   },
   savedjobs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Job' ,required: false}],
 
-  company: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Company' ,required: true}], 
+  compan: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Company' ,required: true}], 
 
 
 
