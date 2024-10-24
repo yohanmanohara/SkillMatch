632cde5b971f761b9fe60d763aff81cbabb2e2cf
yohanmanohara <warushayohan80@gmail.com>
2024-10-24 16:24:19 +0530

    #

---
+++ client/.husky/husky.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# husky.sh - script to be sourced by hooks
+# This is necessary for Husky to function properly.
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# If you need to customize the behavior of husky, add your code here.
+
+# Add additional scripts as needed.
