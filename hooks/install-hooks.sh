a2c8355b3db02e61c3fb4168d169d59e4c564c24
yohanmanohara <warushayohan80@gmail.com>
2024-10-24 15:57:56 +0530

    #

--- hooks/install-hooks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Installing Git hooks..."
-
-# Copy all scripts from the hooks directory to .git/hooks
-cp hooks/* .git/hooks/
-chmod +x .git/hooks/*  # Make hooks executable
-
-echo "Git hooks installed successfully."
