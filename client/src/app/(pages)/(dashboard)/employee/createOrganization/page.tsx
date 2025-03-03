04eb6162456da9ac4f94e48850dc331876e30344
yohanmanohara <warushayohan80@gmail.com>
2025-03-03 20:36:32 +0530

    fix(20): make create organization

--- client/src/app/(pages)/(dashboard)/employee/createOrganization/page.tsx
+++ client/src/app/(pages)/(dashboard)/employee/createOrganization/page.tsx
@@ -64,15 +64,8 @@ export default function TabsDemo() {
   const fileInputRef = useRef<HTMLInputElement | null>(null);
   const [uploaded, setuploaded] = useState(false);
   const [picture, setPicture] = useState("");
-  const role = sessionStorage.getItem('role');
-
-  useEffect(() => {
-
-    if (role == 'Employer') {
-      router.push("/employee/overview");
-    }
-
-  }, []);
+  const [role, setRole] = useState<string | null>(null); 
+  
 
   const handleFileUpload = async () => {
     setLoading(true);
@@ -316,6 +309,21 @@ export default function TabsDemo() {
 
 
   useEffect(() => {
+    const storedRole = sessionStorage.getItem("role");
+
+    if (storedRole) {
+      // Parse the role object if it's a JSON string
+      const parsedRole = JSON.parse(storedRole);
+
+      console.log(parsedRole); // Logs the parsed role object
+
+      if (parsedRole?.role === 'Employer') {
+        router.push("/employee/overview");
+      }
+    } else {
+      setRole(storedRole); // Update state with stored role
+    }
+
     const fetchLocations = async () => {
       try {
         const response = await fetch(
