6692d48488d33872ab73d4e4851856c877f6b734
Vismitha Gunasekara <Vismitha65mya@gmail.com>
2025-03-11 12:29:58 +0530

    footer components added

---
+++ client/src/app/(landing)/footer_components/employers/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+
+const Employers = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-[#92EDAD] text-black p-6">
+      <h1 className="text-3xl font-bold mb-4">Employers</h1>
+      <p className="text-lg">Welcome to the Employers page! Here, you can find details about hiring and recruitment.</p>
+    </div>
+  );
+};
+
+export default Employers;
